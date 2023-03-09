78dcfa411df499cea48a5673329d8e8b32784134
Matu36 <matipineda85@live.com.ar>
2023-03-09 20:53:48 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {PORT} = process.env;
 //const {getApi} = require ("./src/controllers/country.js")
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
