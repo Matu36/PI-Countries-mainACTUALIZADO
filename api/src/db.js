8c9447aab25464eb26090acdccadd345070025f3
Matu36 <matipineda85@live.com.ar>
2023-03-09 09:58:15 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_URL
-} = process.env;
+  DB_URL}
+= process.env;
 
-const sequelize = new Sequelize(DB_URL, {
+const sequelize = new Sequelize(`postgresql://postgres:o1E8YZoRdtiYScE1OvF2@containers-us-west-159.railway.app:7158/railway`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
