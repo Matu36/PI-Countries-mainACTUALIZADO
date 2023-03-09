95548e063896ed365e94d767bc75f47ca9012950
Matu36 <matipineda85@live.com.ar>
2023-03-09 20:12:11 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,12 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgresql://postgres:o1E8YZoRdtiYScE1OvF2@containers-us-west-159.railway.app:7158/railway`, {
+/*const sequelize = new Sequelize(`postgresql://postgres:o1E8YZoRdtiYScE1OvF2@containers-us-west-159.railway.app:7158/railway`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});*/
+
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
