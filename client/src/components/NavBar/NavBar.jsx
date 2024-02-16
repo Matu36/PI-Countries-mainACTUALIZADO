eee4a8f5e8bca752ebfdd241c280edb1e16ed586
Matu36 <matipineda85@live.com.ar>
2024-02-16 20:42:03 -0300

    pais

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../NavBar/NavBar.module.css";
+import { Link } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+
+export default function NavBar() {
+  return <div className={style.Nav}></div>;
+}
